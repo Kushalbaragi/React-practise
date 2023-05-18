08424dab082e6a9dc8c9fbf57f6cc87ebd4cfb8d
Kushal baragi <Kushalbaragi83@gmail.com>
2023-05-18 14:06:32 +0530

    concept part -1

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -3,13 +3,29 @@ import Hello from './hello';
 import ClassComponent from './classComponent';
 import FunctionComponent from './functionComponent';
 import ClickEvent from './clickEvent';
+import EventBind from './eventBind';
+import ClassComponent1 from './ClassComponet1';
+import MethodProps from './MethodsProps';
 
 function App() {
+  let a=10;
+  function cccc(num1= 2, num2=4){
+      console.log(num1+num2);
+    }
+ 
   return (
+
+    
     <>
    {/* <ClassComponent  name='kushal1' lastname='Baragi2'/>
    <FunctionComponent  name='kushal' lastname='Baragi'/> */}
-   <ClickEvent />
+   {/* <ClickEvent /> */}
+   {/* <button onClick={()=>{
+    cccc(10,10,10);
+   }}>click me to add component</button> */}
+   {/* <ClassComponent1/> */}
+
+   <MethodProps/>
 
     </>
     
--- first-app/src/clickEvent.jsx
+++ first-app/src/clickEvent.jsx
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
+import './style.css';
 
-class ClickEvent extends React.Component{
+class ClickEvent extends Component{
     handler(){
         console.log('this is event handler');
     }
     render(){
         return(
             <>
+            <div className='container'>
+                <div className="section">
             <h1>Class component</h1>
             <button onClick={this.handler}>click me</button>
+            </div>
+            </div>
             </>
         )
     }
